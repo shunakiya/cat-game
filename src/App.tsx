e105c01e35dfe56c0430483be9f34051a2b30f5c
shunakiya <shunakiya@gmail.com>
2024-11-26 15:44:49 -0800

    added cat buttons with fixed colors, names change on refresh

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,7 @@ const getRandomCat = () => {
   let list_of_cats: Array<object> = [];
 
   for (let i = 0; i < 4; i++) {
-    let random_value: number = Math.floor(Math.random() * (67 - 0 + 1)) + 0;
-    console.log(random_value);
+    let random_value: number = Math.floor(Math.random() * (66 - 0 + 1)) + 0;
     let random_cat: object = breeds[random_value];
     list_of_cats.push(random_cat);
   }
@@ -13,19 +12,40 @@ const getRandomCat = () => {
 };
 
 function chosenCat(cats: any) {
-  let random_index_value: number = Math.floor(Math.random() * (4 - 0 + 1)) + 0;
+  let random_index_value: number = Math.floor(Math.random() * 4);
   return cats[random_index_value];
 }
 
 export default function App() {
-  // let key =
-  //   "api_key=live_TaVgvxxbCwoPa7IGScs9j0a6dHzajK39eTjH6srcQEA07BvPkLihD8F7zEbM0MRq";
-  let cats: object = getRandomCat();
+  let cats: Array<any> = getRandomCat();
   console.log(chosenCat(cats));
+
+  const buttonColors = [
+    "bg-red-400",
+    "bg-yellow-400",
+    "bg-blue-400",
+    "bg-green-400",
+  ];
 
   return (
     <>
       <p className="text-3xl">Cat Game</p>
+      <div className="mt-4">
+        <ul>
+          {cats.map((cat, index) => (
+            <li key={index} className="mb-2">
+              <button
+                className={`border-[1px] py-2 px-2 rounded-xl shadow-xl text-zinc-700 ${
+                  buttonColors[index % buttonColors.length]
+                }`}
+                key={index}
+              >
+                {cat.name}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
