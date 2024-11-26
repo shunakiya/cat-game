7a43b25a9edeacee56df9a0fd89c2e8dde531f29
shunakiya <shunakiya@gmail.com>
2024-11-25 16:59:28 -0800

    testing cat fetching logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import breeds from "./assets/breeds.json";
+
+const getRandomCat = () => {
+  let list_of_cats: Array<object> = [];
+
+  for (let i = 0; i < 4; i++) {
+    let random_value: number = Math.floor(Math.random() * (67 - 0 + 1)) + 0;
+    let random_cat = breeds[random_value];
+    list_of_cats.push(random_cat);
+  }
+  return list_of_cats;
+};
+
 export default function App() {
+  // let key =
+  //   "api_key=live_TaVgvxxbCwoPa7IGScs9j0a6dHzajK39eTjH6srcQEA07BvPkLihD8F7zEbM0MRq";
+  console.log(getRandomCat());
+
   return (
     <>
-      <p className="">hello</p>
+      <p className="text-3xl">Cat Game</p>
     </>
   );
 }
