a83b81253a7cbf6acaf6cd4b923b846a9ec75984
shunakiya <shunakiya@gmail.com>
2024-11-25 22:50:59 -0800

    added chosen cat

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,23 @@ const getRandomCat = () => {
 
   for (let i = 0; i < 4; i++) {
     let random_value: number = Math.floor(Math.random() * (67 - 0 + 1)) + 0;
-    let random_cat = breeds[random_value];
+    console.log(random_value);
+    let random_cat: object = breeds[random_value];
     list_of_cats.push(random_cat);
   }
   return list_of_cats;
 };
 
+function chosenCat(cats: any) {
+  let random_index_value: number = Math.floor(Math.random() * (4 - 0 + 1)) + 0;
+  return cats[random_index_value];
+}
+
 export default function App() {
   // let key =
   //   "api_key=live_TaVgvxxbCwoPa7IGScs9j0a6dHzajK39eTjH6srcQEA07BvPkLihD8F7zEbM0MRq";
-  console.log(getRandomCat());
+  let cats: object = getRandomCat();
+  console.log(chosenCat(cats));
 
   return (
     <>
