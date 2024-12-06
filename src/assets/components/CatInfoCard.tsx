e81f99dbeea5a625415f074bfb0f075c3e464662
shunakiya <shunakiya@gmail.com>
2024-12-05 16:00:06 -0800

    beginning to make catinfocard (shows right cat to user after answer)

---
+++ src/assets/components/CatInfoCard.tsx
@@ -0,0 +1,12 @@
+import { Cat } from ".../Cat.ts";
+import React from "react";
+
+interface CatInfoCardProps {
+  cat: Cat;
+  isCorrect: boolean;
+}
+
+// const CatInfoCard: React.FC<CatInfoCardProps> = ({ cat, isCorrect }) => {
+
+// };
+// export default CatInfoCard;
